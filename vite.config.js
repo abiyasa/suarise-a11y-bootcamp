ddc59d202aaf845ed70a4540f91c2bf14166b492
abiyasa <ilham.suhardi@gmail.com>
2024-02-16 12:37:03 +0100

    Update vite config
    to publish multi-page app

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        '01-simple-counter': resolve(__dirname, '01-simple-counter/index.html'),
+        '02-focus-order': resolve(__dirname, '02-focus-order/index.html'),
+        '02-skip-nav': resolve(__dirname, '02-skip-nav/index.html'),
+        '03-managing-focus': resolve(__dirname, '03-managing-focus/index.html'),
+        '03-modals': resolve(__dirname, '03-modals/index.html'),
+        '04-radio-group': resolve(__dirname, '04-radio-group/index.html'),
+        '04-radio-group-native': resolve(__dirname, '04-radio-group/index-native.html'),
+      }
+    }
+  }
+});
